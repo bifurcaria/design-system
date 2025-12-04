c5e9f60e51a31a7267c0bd8ef6516a8a00ac1b25
carla <106553998+bifurcaria@users.noreply.github.com>
2025-12-04 20:36:59 -0300

    feat(web): consume ui package

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from '@design-system/ui'
 import './App.css'
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <Button />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
