d6b273ed688826b080199912ff97884a42dada50
carla <106553998+bifurcaria@users.noreply.github.com>
2025-12-05 12:22:24 -0300

    feat(typography): types for typography components

---
+++ packages/ui/src/components/Typography/types.ts
@@ -0,0 +1,31 @@
+import { ReactNode } from "react";
+
+export interface BaseTypographyProps {
+  children: ReactNode;
+  align?: Extract<React.CSSProperties['textAlign'], 'left' | 'center' | 'right'> | 'auto';
+  sensitive?: boolean;
+  flexShrink?: number;
+  flexGrow?: number;
+}
+
+  interface HeadingProps extends BaseTypographyProps {
+    variant?: HeadingVariants; 
+    bold?: never; 
+  }
+  
+  interface BodyProps extends BaseTypographyProps {
+    variant?: BodyVariants; 
+    bold?: boolean;
+  }
+
+  interface LabelProps extends BaseTypographyProps {
+    variant?: LabelVariants;
+    bold?: never;
+  }
+  
+  type LabelVariants = 'labelLarge' | 'label' | 'labelSmall';
+  type BodyVariants = 'body' | 'caption' | 'footnote';
+  type HeadingVariants = 'display' | 'titleLarge' | 'title' | 'titleSmall' | 'headline' | 'subheadline';
+
+  export type TypographyProps = HeadingProps | BodyProps | LabelProps;
+  export type TypographyVariant = LabelVariants | BodyVariants | HeadingVariants;
