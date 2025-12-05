1af6697222c4b83e96e08f68b040a11e5afc48c2
carla <106553998+bifurcaria@users.noreply.github.com>
2025-12-05 20:20:14 -0300

    chore(fonts): create font utility to manage font assets in mobile

---
+++ packages/ui/src/utils/fonts.ts
@@ -0,0 +1,9 @@
+import PoppinsRegular from '../assets/fonts/Poppins-Regular.ttf';
+import PoppinsMedium from '../assets/fonts/Poppins-Medium.ttf';
+import PoppinsBold from '../assets/fonts/Poppins-Bold.ttf';
+
+export const fontAssets = {
+  'Poppins-Regular': PoppinsRegular,
+  'Poppins-Medium': PoppinsMedium,
+  'Poppins-Bold': PoppinsBold,
+};
