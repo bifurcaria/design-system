19e113847f0630f94f37a45041b8520aa20b4303
carla <106553998+bifurcaria@users.noreply.github.com>
2025-12-05 20:26:42 -0300

    chore(apps): remove unrelated stuff from both apps

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,13 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import { Button, Typography } from '@design-system/ui';
-import { useState } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { Typography } from '@design-system/ui';
 import { useFonts } from 'expo-font';
 import { fontAssets } from '@design-system/ui/src/utils/fonts';
 
 export default function App() {
   const [fontsLoaded] = useFonts(fontAssets);
-  const [count, setCount] = useState(0);
   
   if (!fontsLoaded) {
     return <ActivityIndicator size="small" />
@@ -15,13 +12,7 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button onPress={() => setCount((count) => count + 1)}>
-        <Text>Click me</Text>
-      </Button>
-      <Text>Count: {count}</Text>
-      <Typography>Hi :-) this is a typography component in native.</Typography>
+      <Typography>Hi! This is a typography component in native.</Typography>
     </View>
   );
 }
--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,36 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button, Typography } from "@design-system/ui"
+import { Typography } from "@design-system/ui"
 import './App.css'  
 
-function App() {
-  const [count, setCount] = useState(0)
+    function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onPress={() => setCount((count) => count + 1)}>
-          Click me
-        </Button>
-        <span>
-          count is {count}
-        </span>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+    <div>
       <Typography>Hi! This is a typography component.</Typography>
-    </>
+    </div>
   )
 }
 
